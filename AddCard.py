9f893216f36dfb99f2fd80cce4fd724ef0d6213b
apiradee-mai-PC <apiradee-mai-PC@users.noreply.github.com>
2018-08-16 14:02:51 +0700

    input key and token

--- AddCard.py
+++ AddCard.py
@@ -4,7 +4,7 @@ list =  input("Which list you want to create card: ")
 
 if (list!='To Do' and list!='Doing' and list!='Done'):
 	print("\nError List!")
-
+	exit(1)
 if list=='To Do':
 	idList = '5b6c95b404562869fba22f6b'
 
@@ -13,15 +13,21 @@ elif list=='Doing':
 	
 elif list=='Done':
 	idList = '5b6c95b404562869fba22f6d'
+	
+key =  input("API Keys: ")
+token =  input("Token: ")
+cardName =  input("Input card Name: ")
+desc = input("input card description: ")
 
-
-
+	
 
 url = "https://api.trello.com/1/cards"
 
-querystring = {"name":"TestName","desc":"TestDesc","pos":"top","due":"2018-08-16","idList":"5b6c95b404562869fba22f6b","urlSource":"https://","keepFromSource":"all"}
+querystring = {"key":key,"token":token,"name":cardName,"desc":desc,"idList":idList}
 
 response = requests.request("POST", url, params=querystring)
 
 print(response.text)
+
+
 
